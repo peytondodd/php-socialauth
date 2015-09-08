144c57786a161216db405d85c887dde6fd709126
Sohel Rana <me.sohelrana@gmail.com>
2015-09-08 18:31:43 +0600

    Class has been mapping through PSR-4 and it's preper for working

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require 'vendor/autoload.php';
+$config = new \SocialAuth\Config();
+echo $config->name;
---
+++ src/Config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author: Sohel Rana
+ * @email: me.sohelrana@gmail.com
+ * @uri: http://sohelrana.me
+ * @tags: social login, php social login, login with facebook, login with twitter, login with google. login with
+ * linkedin
+ */
+
+
+namespace SocialAuth;
+
+class Config
+{
+    public $name = 'SocialAuth';
+}
