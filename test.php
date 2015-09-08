64eb78fa8cb9d4c0d1400a208be6f0c0166f8b82
Sohel Rana <me.sohelrana@gmail.com>
2015-09-08 18:06:12 +0600

    test page created

---
+++ test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login with Social Media</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="well" style="margin-top: 100px;">
+        Login with Social Media
+    </div>
+</div>
+
+</body>
+</html>
