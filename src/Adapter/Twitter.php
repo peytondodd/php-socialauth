aee40e7198aebbc0234128fe434b8919cc24be8a
Sohel Rana <me.sohelrana@gmail.com>
2015-09-17 17:58:31 +0600

    Changed the twitter sdk files

--- src/Adapter/Twitter.php
+++ src/Adapter/Twitter.php
@@ -2,7 +2,6 @@
 
 namespace SocialAuth\Adapter;
 
-use Abraham\TwitterOAuth\TwitterOAuth;
 use SocialAuth\Config;
 use SocialAuth\Utility\ErrorHandler;
 
@@ -21,16 +20,49 @@ class Twitter
     public function __construct($callbackUrl)
     {
         $this->callBackUrl = $callbackUrl;
-        $this->twitter = new TwitterOAuth(Config::$twitterCustomerKey, Config::$twitterCustomerSecretKey, Config::$twitterAccessToken, Config::$twitterAccessTokenSecret);
+
+        $settings = array(
+            'oauth_access_token' => Config::$twitterAccessToken,
+            'oauth_access_token_secret' => Config::$twitterAccessTokenSecret,
+            'consumer_key' => Config::$twitterCustomerKey,
+            'consumer_secret' => Config::$twitterCustomerSecretKey
+        );
+
+        $this->twitter = new \TwitterAPIExchange($settings);
+    }
+
+    public function buildApiUrl($urlPath)
+    {
+        return 'https://api.twitter.com/'.$urlPath.'.json';
     }
 
     public function checkAuth()
     {
-        $verified = $this->twitter->get("account/verify_credentials");
-        if($verified)
-        {
-            return true;
-        }
-        return false;
+        $response = $this->twitter->setGetfield()
+            ->buildOauth($this->buildApiUrl('account/verify_credentials'), 'GET')
+            ->performRequest();
+
+        $url = $this->buildApiUrl('oauth/request_token');
+        $requestMethod = 'POST';
+
+        var_dump($url);
+
+        $postfields = array(
+            'OAuth oauth_nonce' => time(),
+            'oauth_callback' => $this->callBackUrl,
+            'oauth_signature_method' => "HMAC-SHA1",
+            'oauth_timestamp' => strtotime(time()),
+            'oauth_consumer_key' => Config::$twitterCustomerKey,
+            /*'oauth_signature' => "Pc%2BMLdv028fxCErFyi8KXFM%2BddU%3D",*/
+            'oauth_signature' => "1.0"
+        );
+
+        var_dump($postfields);
+
+        $response = $this->twitter->buildOauth($url, $requestMethod)
+            ->setPostfields($postfields)
+            ->performRequest();
+
+        var_dump($response);
     }
 }
